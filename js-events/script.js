1933c53113c68bbf0ab5069d35027eb441940da7
Jazmin Gomez <gomjazmin12@gmail.com>
2026-04-17 17:29:07 +0000

    .....

---
+++ js-events/script.js
@@ -0,0 +1,18 @@
+// function greet(name){
+//     console.log("Hello " + name);
+// }
+
+// greet("Jazmin");
+
+// function addNumbers(num1, num2){
+//     let sum = num1 + num2;
+//     alert("Sum is equal to " + sum);
+// }
+
+// addNumbers(6,7);
+
+function Article(){
+    alert("1 Nosotros creemos en Dios el Eterno Padre, y en su Hijo Jesucristo, y en el Espíritu Santo.");
+
+}
+Article(")
