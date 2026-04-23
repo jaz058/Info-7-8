8c0c2e42795dcea5c5cf67168a3d93354c79cd6a
Jazmin Gomez <gomjazmin12@gmail.com>
2026-04-23 20:01:16 +0000

    ,,,,,

--- js-variables/script.js
+++ js-variables/script.js
@@ -1,25 +1,33 @@
 function function1(){
-    const name = "Jazmin"
-    const color= "Pink"
-    let location= "Academia Juarez"
-    let mood = "Enojada"
-    console.log(Name)
-    console.log(Color)
-    console.log(Location)
-    console.log(Mood)
+    const name = "Jazmin";
+    const color= "Pink";
+    let location= "Academia Juarez";
+    let mood = "Enojada";
+    console.log(name);
+    console.log(color);
+    console.log(location);
+    console.log(mood);
 }
 
 
 function function2(){
-    companyName = "Coca cola"
-    foundingYear = "1886"
-    isActive = True
-    fundingAmount= "8 /05/1886"
-    console.log(companyName)
-    console.log(foundingYear)
-    console.log(isActive )
-    console.log(fundingAmount)
+    companyName = "Coca cola";
+    foundingYear = "1886";
+    isActive = "True";
+    fundingAmount= "8 /05/1886";
+    console.log(companyName);
+    console.log(foundingYear);
+    console.log(isActive );
+    console.log(fundingAmount);
 }
 
+
+function function3(){
+     let number= 6;
+     number = number*2;
+     number = number+10;
+     number = number/2;
+     alert("The varible value should be "+number );
+}
 
 
