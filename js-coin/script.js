bccd1f8afef3d650b136dc59c35100bb0f1b3281
Jazmin Gomez <gomjazmin12@gmail.com>
2026-04-28 16:39:47 +0000

    ññññ

--- js-coin/script.js
+++ js-coin/script.js
@@ -1,7 +1,12 @@
-function coin{
+function coin(){
     let randomNum = Math.random();
-    console.log(randomNum);
+    let subtitle= document.querySelector("h2");
 
-    let subtitle= document. querySelector("h2");
-    if (randomNum)
+if (randomNum < 0.5){
+    subtitle.innerHTML = "Heads";
+}
+else {
+    subtitle.innerHTML = "Tails";
+}
+
 }
