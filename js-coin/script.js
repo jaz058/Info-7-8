f67316aad215755259491beb5ec3e404e04e2e30
Jazmin Gomez <gomjazmin12@gmail.com>
2026-04-30 20:19:40 +0000

    jjjjj

--- js-coin/script.js
+++ js-coin/script.js
@@ -1,34 +1,20 @@
-// function coin(){
-//     let randomNum = Math.random();
-//     let subtitle= document.querySelector("h2");
 
-// if (randomNum < 0.5){
-//     subtitle.innerHTML = "Heads";
-// }
-// else {
-//     subtitle.innerHTML = "Tails";
-// }
-
-// }
-function options(){
-    if ( )
-}
 function head() {
-let random = Math.random();
-let text = document.querySelector("h4");
+    let random = Math.random();
+    let text = document.querySelector("h1");
 
-if (random < 0.5){
-text.innerHTML = "Congratulations, You Won!!!";
-}
-else {
-text.innerHTML = "Sorry but, You Loose";
-}
+    if (random < 0.5){
+        text.innerHTML = "Congratulations, You Won!!!";
+    }
+    else {
+        text.innerHTML = "Sorry but, You Loose";
+    }
 }
 
 
 function tail() {
 let random = Math.random();
-let text = document.querySelector("h4");
+let text = document.querySelector("h2");
 
 if (random > 0.5){
 text.innerHTML = "Congratulations, You Won!!!";
