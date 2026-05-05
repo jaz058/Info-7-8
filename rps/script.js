2494cec5c2bf31aa37446f277c5aa064116c6bdc
Jazmin Gomez <gomjazmin12@gmail.com>
2026-05-05 16:29:35 +0000

    mmmm

--- rps/script.js
+++ rps/script.js
@@ -1,36 +1,64 @@
-function game(){
-    if ( )
-}
-function rock() {
-    let random = Math.random();
-    let text = document.querySelector("h2");
-    if (random < 0.5){
-        text.innerHTML = "Congratulations, You Won!!!";
+funtion game (userChoice){
+    let randomNum= Math.random();
+    let compChoice;
+    if (randomNum < 0.33){
+        compChoice = "Rock";
+    } else if (randomNum < 0.66){
+        compChoice = "Paper"
+    } else if (randomNum < 1){
+         compChoice = "Scissors";
     }
-    else {
-        text.innerHTML = "Sorry but, You Loose";
+
+    console.log(compChoice)
+    let result= document.querySelector
+    ("#result");
+    if (userChoice==compChoice){
+        result.innerHTML = "Tie";
+    }else if (userChoice == "Rock"){
+        if (compChoice == "Paper"){
+            result.innerHTML = "You lose";
+        } else {
+            result.innerHTML ="You won";
+        }
     }
 }
 
-function paper() {
-    let random = Math.random();
-    let text = document.querySelector("h2");
-    if (random > 0.5){
-        text.innerHTML = "Congratulations, You Won!!!";
-    }
-    else {
-        text.innerHTML = "Sorry but, You Loose";
-    }
 }
+    // document.addEventListener('DOMContenttLoaded', function(){
+    //     //Your code here
+    //     document.querySelectpr("#rock-button").onclick = game("Rock");
+    // }
+
+// function rock() {
+//     let random = Math.random();
+//     let text = document.querySelector("h2");
+//     if (random < 0.5){
+//         text.innerHTML = "Congratulations, You Won!!!";
+//     }
+//     else {
+//         text.innerHTML = "Sorry but, You Loose";
+//     }
+// }
+
+// function paper() {
+//     let random = Math.random();
+//     let text = document.querySelector("h2");
+//     if (random > 0.5){
+//         text.innerHTML = "Congratulations, You Won!!!";
+//     }
+//     else {
+//         text.innerHTML = "Sorry but, You Loose";
+//     }
+// }
 
 
-function scissors() {
-    let random = Math.random();
-    let text = document.querySelector("h2");
-    if (random > 0.5){
-        text.innerHTML = "Congratulations, You Won!!!";
-    }
-    else {
-        text.innerHTML = "Sorry but, You Loose";
-    }
-}
+// function scissors() {
+//     let random = Math.random();
+//     let text = document.querySelector("h2");
+//     if (random > 0.5){
+//         text.innerHTML = "Congratulations, You Won!!!";
+//     }
+//     else {
+//         text.innerHTML = "Sorry but, You Loose";
+//     }
+// }
