4593bbb3a40b0c82b7e64fc630266bd0bb0a4a3e
Jazmin Gomez <gomjazmin12@gmail.com>
2026-05-07 17:28:57 +0000

    lllll

--- greeting/script.js
+++ greeting/script.js
@@ -1,8 +1,27 @@
-document.addEventListener("DOMContentLoaded"; function()
+document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("form").onsubmit = function (){
+
         let name = document.querySelector("#name").value;
         console.log("Hi " + name);
 
+        document.querySelector("h1").innerHTML = "Hi " + name;
+
+        let now = new Date();
+
+        let hour= now.getHours();
+        if (hour < 12){
+            document.querySelector("#time").innerHTML = "☀️Good Morning!";
+
+        } else if(hour < 18){
+            document.querySelector("#time").innerHTML = "🌄Good Afternoon!";
+
+        } else{
+            document.querySelector("#time").innerHTML = "🌙Good Night!";
+
+        }
+
+        let day= now.getDays
         
         return false;
-}
+    };
+});
